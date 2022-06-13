afcf872f8e9b8fc6c0b470761e8bf7dbc960b337
YangxinMa <wghmyx@gmail.com>
2022-06-13 16:33:22 -0700

    login component

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Login extends React.Component {
+  render() {
+    return <p>Login Component</p>;
+  }
+}
+
+ReactDOM.render(<Login />, document.getElementById("root"));
